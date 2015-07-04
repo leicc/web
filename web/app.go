ceacd53dc0e352ca4199aa274e86592e62551a11
leicc <lchenchun@sina.com>
2015-07-04 18:12:23 +0800

    aaaaaaaaaa

--- web/app.go
+++ web/app.go
@@ -3,19 +3,26 @@ package main
 import (
 	"fmt"
 	"git.lcc.lib/core"
-	//"time"
+	"git.lcc.lib/verify"
 )
 
 func main() {
+	log := core.NewLoger("./cache/log", 2)
+	fmt.Println(log)
+	core.Log(1, "1111", "aaaaaaa", "cccccccccccc")
+	core.Log(2, "2222", "aaaaaaa", "cccccccccccc")
+	core.Log(3, "2222", "aaaaaaa", "cccccccccccc")
 
-	ini := core.NewIni("config/web.ini")
-	fmt.Println(ini.GetItem("SERVER", "Host"))
+	core.Logf(1, "%s-%s-%s", "333333333", "aaaaaaa", "cccccccccccc")
+	core.Logf(4, "%s-%s-%s", "333333333", "aaaaaaa", "cccccccccccc")
 
-	ini.ReLoad()
-	fmt.Println(ini.GetItem("REDIS", "Host"))
+	fmt.Println(verify.Email("lchenchun@sina.com.12"))
 
-	fmt.Println(ini.GetItem("LOGER", "Mask"))
-	fmt.Println(ini.GetItem("LOGER", "Dir"))
+	fmt.Println(verify.Phone("135140768061"))
 
-	//time.Sleep(time.Second * 30)
+	fmt.Println(verify.Numeric("13aa516"))
+
+	fmt.Println(verify.Alpha("13516"))
+
+	fmt.Println(verify.Ip("1a1.0.11.0"))
 }
